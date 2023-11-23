18a0d94178d81545234db0879d2d1daff199f7e5
Marsh1100 <margie.b.agu17@gmail>
2023-11-23 14:46:46 -0500

    Unidad de trabajo

---
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces;
+
+public interface IUnitOfWork{
+    IClient Clients { get; }
+    IEmployee Employees { get; }
+    IOffice Offices {   get; }
+    IPayment Payments   { get; }
+    IProduct Products { get; }
+    IProducttype Producttypes { get; }
+    IRequest Requests { get; }
+    IRequestdetail Requestdetails { get; }
+    Task<int> SaveAsync();
+
+}
